4899c6886fd8797efd4f85ac63fe80b33091d536
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-10-16 17:26:06 +0530

    chore: add functionality and dependencies

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-# hello-world
+# wgit
 
-![Build and Deploy](https://github.com/rajasegar/node-cli-template/workflows/Build%20and%20Deploy/badge.svg)
+![Build and Deploy](https://github.com/rajasegar/wgit/workflows/Build%20and%20Deploy/badge.svg)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
-[![npm version](http://img.shields.io/npm/v/node-cli-template.svg?style=flat)](https://npmjs.org/package/node-cli-template "View this project on npm")
-<A description about your CLI>
+[![npm version](http://img.shields.io/npm/v/wgit.svg?style=flat)](https://npmjs.org/package/wgit "View this project on npm")
 
-## How to use this template?
-Use [degit](https://github.com/Rich-Harris/degit) to install the template in your local machine
-```
-degit rajasegar/node-cli-template
-```
+Copy-paste your files from Github repos to local.
 
 ## Install
 ```
-npm install -g hello-world
+npm install -g wgit
 ```
 
 ## Usage
 ```
-hello-world
+wgit
 ```
 
 You can also use npx to run:
 ```
-npx hello-world
+npx wgit
 ```
--- index.js
+++ index.js
@@ -1,5 +1,71 @@
 'use strict';
 
+const prompts = require('prompts');
+const fs = require('fs');
+
 module.exports = function () {
-  console.log('Hello world!');
+  const starterPrompt = [
+    {
+      type: 'text',
+      name: 'owner',
+      message: 'Enter github user id:',
+    },
+    {
+      type: 'text',
+      name: 'repo',
+      message: 'Enter the repository name:',
+    },
+  ];
+
+  (async () => {
+    let answers = await prompts(starterPrompt);
+    const { owner, repo } = answers;
+    require('dotenv').config();
+    const { Octokit } = require('@octokit/rest');
+    const octokit = new Octokit({ auth: process.env.GH_PAT });
+
+    const response = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: '',
+    });
+
+    const _choices = response.data
+      .filter((f) => f.type === 'file')
+      .map((f) => {
+        return { title: f.name, value: f.path };
+      });
+
+    const choosePrompt = {
+      type: 'multiselect',
+      name: 'files',
+      message: 'Choose files to copy:',
+      choices: _choices,
+    };
+
+    answers = await prompts(choosePrompt);
+    const { files } = answers;
+
+    files.forEach((file) => {
+      octokit.repos
+        .getContent({
+          owner,
+          repo,
+          path: `${file}`,
+        })
+        .then((response) => {
+          const fileData = Buffer.from(response.data.content, 'base64');
+          console.log(fileData.toString());
+
+          // Write file
+          fs.writeFile(`${file}`, fileData, (err) => {
+            if (!err) {
+              console.log(`${file} written successfully.`);
+            } else {
+              console.log(err);
+            }
+          });
+        });
+    });
+  })();
 };
